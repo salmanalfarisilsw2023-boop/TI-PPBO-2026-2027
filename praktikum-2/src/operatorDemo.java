ab51b12956d216de2221d0f9eebd115a4348ec42
salman alfarisi <salmanalfarisi.lsw2023@gmail.com>
2026-09-08 10:50:26 +0700

    B.3. Operator

---
+++ praktikum-2/src/operatorDemo.java
@@ -0,0 +1,10 @@
+public class operatorDemo {
+    public static void main(String[] args) {
+       int a = 17, b = 5;
+       System.out.println("jumlah: " + (a + b));
+        System.out.println("selisih: " + (a - b));
+        System.out.println("kali: " + (a * b));
+        System.out.println("bagi: " + (a / b));
+        System.out.println("sisa bagi: " + (a % b));
+    }
+}
