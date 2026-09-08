3201b28df1a0d26a5d582b2969f3e3ced487e201
salman alfarisi <salmanalfarisi.lsw2023@gmail.com>
2026-09-08 09:48:30 +0700

    A.2. Struktur Dasar Program java

--- praktikum-2/src/helloworld.java
+++ praktikum-2/src/helloworld.java
@@ -1,5 +1,10 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Halo, Dunia!");
+        System.out.println("Halo, salman!");
+        System.out.print("Nama: ");
+        System.out.print("salman alfarisi");
+        System.out.println();
+        System.out.println("Ini baris baru");
+
     }
 }
