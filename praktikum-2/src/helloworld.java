cf14716dead4bf890d0d711ecbebb4651ac786b0
salman alfarisi <salmanalfarisi.lsw2023@gmail.com>
2026-09-08 10:22:14 +0700

    A.3. Komentar Kode

--- praktikum-2/src/helloworld.java
+++ praktikum-2/src/helloworld.java
@@ -1,9 +1,12 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Halo, salman!");
+        // program menampilkan biodata sederhana
         System.out.print("Nama: ");
         System.out.print("salman alfarisi");
+        /* baris di bawah ini mencetak NIM mahasiswa */
         System.out.println();
+        System.out.println("NIM: 2025573010130");
         System.out.println("Ini baris baru");
 
     }
