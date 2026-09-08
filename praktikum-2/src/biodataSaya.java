5c6410baee25073221073bb255cf304709f04a76
salman alfarisi <salmanalfarisi.lsw2023@gmail.com>
2026-09-08 11:37:00 +0700

    Latihan 2 BiodataSaya

--- praktikum-2/src/biodataSaya.java
+++ praktikum-2/src/biodataSaya.java
@@ -1,10 +1,15 @@
 public class biodataSaya {
     public static void main(String[] args) {
-        // program menampilkan biodata sederhana
+        // cetak dengan println()
         System.out.println("Nama: salman alfarisi");
         System.out.println("NIM: 2025573010130");
-        /*baris di bawah ini mencetak program studi*/
         System.out.println("Program Studi: Teknik Informatika");
+        System.out.println(); // baris kosong pemisah
 
+        // cetak nama dan nim satu baris di pisahkan tanda'-'
+        System.out.println("salman alfarisi");
+        System.out.println(" - ");
+        System.out.println("2025573010130");
+        System.out.println();
     }
 }
