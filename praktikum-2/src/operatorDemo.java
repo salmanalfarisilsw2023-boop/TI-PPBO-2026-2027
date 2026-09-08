7dee6e13ad60cccecd95250ad30bb3b7d434dd9e
salman alfarisi <salmanalfarisi.lsw2023@gmail.com>
2026-09-08 10:58:03 +0700

    B.3. Operator

--- praktikum-2/src/operatorDemo.java
+++ praktikum-2/src/operatorDemo.java
@@ -6,5 +6,9 @@ public class operatorDemo {
         System.out.println("kali: " + (a * b));
         System.out.println("bagi: " + (a / b));
         System.out.println("sisa bagi: " + (a % b));
+        int nilaiUjian = 80;
+        boolean hadir = true;
+        System.out.println("lulus (>=75): " + (nilaiUjian >=75));
+        System.out.println("lulus dan hadir: " + (nilaiUjian >=75 && hadir));
     }
 }
